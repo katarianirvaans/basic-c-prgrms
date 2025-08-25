d3bc1864f30c167bb94ba3b5e89c24c26479384e
katarianirvaans <katarianirvaans@gmail.com>
2025-08-25 15:04:48 +0530

    Create prgrm7.c
    7/∞

---
+++ prgrm7.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+//this is prgrm to calculate the remainder when we divide a specific number by 97.
+
+int main(){
+    int x=334989;
+    printf("The remainder when we divide x by 97 is %d" , x%97);
+    return 0;
+}
