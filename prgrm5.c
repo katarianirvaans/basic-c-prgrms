ef5adc3f903d4ae11cadbddc0c7cc99d8c7dd76b
katarianirvaans <katarianirvaans@gmail.com>
2025-08-25 15:02:35 +0530

    Create prgrm5.c
    5/∞

---
+++ prgrm5.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+//This is a prgrm to calculate simple interest where p , r , t are predefined.
+
+int main(){
+     float p = 30;
+    int r = 10;
+    int t=3;
+    printf("the value of simple interest is %f " , (p*r*t)/ 100);
+    return 0;
+}
