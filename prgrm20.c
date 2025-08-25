10eb77b3a28b0b3fdd676f5e8730dcece54c747f
katarianirvaans <katarianirvaans@gmail.com>
2025-08-25 15:23:53 +0530

    Create prgrm20.c
    20/∞

---
+++ prgrm20.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(){
+    int i=0;
+    while(1<12){
+        printf("The value of i is: %d \n" , i);
+        i++;
+    }
+    /*Infinite loop will take place when either i++ is not written or
+     a universal true condition is proposed into the while statement condition.*/
+    return 0;
+}
