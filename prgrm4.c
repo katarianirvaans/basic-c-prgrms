a9c8e351061e065f53b9bc3ec23e71ae4ae4af20
katarianirvaans <katarianirvaans@gmail.com>
2025-08-25 15:01:11 +0530

    Create prgrm4.c
    4/∞

---
+++ prgrm4.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+//This is a prgrm to convert celsius into fareinhiet
+
+int main(){
+  float c=37 ,f ;
+  f = (9.0/5.0)*c +32;
+
+    printf("value in farenheit is %f" , f);
+    return 0;
+}
