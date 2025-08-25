8871c86a7b6f084cefb2620fabeaf2048acd7969
katarianirvaans <katarianirvaans@gmail.com>
2025-08-25 15:22:38 +0530

    Create prgrm19.c
    19/∞

---
+++ prgrm19.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+// for loop.
+
+int main(){
+    int i;
+
+    for ( i = 0; i <=5; i++)
+        {
+                    printf("This is iteration number: %d \n" , i);
+    }
+        printf("The loop has ended! \n");
+    return 0;
+}
