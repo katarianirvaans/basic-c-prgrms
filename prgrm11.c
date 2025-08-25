80b2446c23d39ebdf488caf1caf8b01c99f624a7
katarianirvaans <katarianirvaans@gmail.com>
2025-08-25 15:11:34 +0530

    Create prgrm11.c
    11/∞

---
+++ prgrm11.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+//this prgrm tells us about variables.
+
+int main(){
+    int i=11;
+    int j=i;
+    printf("value of i is %d and the value of j is %d " , i,j );
+    
+    return 0;
+}
