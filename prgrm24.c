633383b3bcbe61ef4833f5a29d61c82cfd272a24
katarianirvaans <katarianirvaans@gmail.com>
2025-08-25 15:28:48 +0530

    Create prgrm24.c
    24/∞

---
+++ prgrm24.c
@@ -0,0 +1,17 @@
+//Write a program to sum first ten natural numbers using while loop.
+
+#include<stdio.h>
+
+int main(){
+    int i=0 ;
+    int sum=0 ;
+    while (i<=10)
+    {
+        sum+=i;
+        i++;
+    }
+    printf("The sum of first 10 natural numbers is: %d \n " , sum);
+    printf("Loop terminated.");
+    
+    return 0;
+}
