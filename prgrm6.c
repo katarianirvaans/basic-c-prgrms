fc1378a11ec03da2478d4396475349b40f80aebb
katarianirvaans <katarianirvaans@gmail.com>
2025-08-25 15:03:40 +0530

    Create prgrm6.c
    6/∞

---
+++ prgrm6.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+//This is a prgrm to calculate difference of 2 constant numbers.
+
+int main(){
+    int a=3;
+    int b=5;
+    int c=b-a;
+    printf("value of a is %d , value of b is %d and their difference is %d", a,b,c);
+    return 0;
+}
