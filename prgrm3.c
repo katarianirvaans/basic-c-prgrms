35dba3608ba507c86303781ec89ad147dbebe51b
katarianirvaans <katarianirvaans@gmail.com>
2025-08-25 14:58:52 +0530

    Create prgrm3.c
    3/∞

---
+++ prgrm3.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+// this is a prgrm to find the area of a circle. (w predefined dimensions)
+
+int main(){
+    int r = 6;
+    int h = 10;
+    printf("the area of circle with radius %d is %f \n" , r, 3.14*r*r);
+    printf("the vol of cylinder with radius %d and hieght %d is %f" , r, h, 3.14*r*r*h);
+    return 0;
+}
