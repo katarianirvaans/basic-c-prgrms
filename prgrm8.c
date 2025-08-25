9d22bbee67e96f1812ee84137f266a88fa68ad68
katarianirvaans <katarianirvaans@gmail.com>
2025-08-25 15:07:23 +0530

    Create prgrm8.c
    8/∞

---
+++ prgrm8.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+//this a prgrm to check the priority order of arithmetic operations.
+
+int main(){
+    int x=2;
+    int y=3;
+    int z=3;
+    int k=1;
+    float a;
+
+
+    //let a = 3*x/y-z+k (where values are predefined)
+    
+    a = 3*x/y-z+k;
+
+    printf("The final result will be %f" , a);
+    return 0;
+}
