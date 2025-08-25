fc3b24110888a8494e9824c7ab97ba259ebbc2e3
katarianirvaans <katarianirvaans@gmail.com>
2025-08-25 15:08:22 +0530

    Create prgrm9.c
    9/∞

---
+++ prgrm9.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+// this is a prgrm to identify the datatype of a variable a.
+
+int main(){
+    float a = 3.0/8-2;
+
+    printf("The datatype of the final result would be %f" , a);
+
+
+    return 0;
+}
