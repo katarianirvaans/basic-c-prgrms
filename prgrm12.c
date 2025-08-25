10abfbd9017839493337a4ae4dbfe72c998729d1
katarianirvaans <katarianirvaans@gmail.com>
2025-08-25 15:13:30 +0530

    Create prgrm12.c
    12/∞

---
+++ prgrm12.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+//this a prgrm about the use of conditionals.
+
+int main(){
+     int x=10;
+    if(x>10){
+        printf("The given age is greater than 10. \n");
+}
+
+ if(x=10){
+        printf("The given age is exactly 10. \n");
+}
+
+    else{
+        printf("The given age isn't greater than 10. \n");
+    }
+
+
+    return 0;
+}
