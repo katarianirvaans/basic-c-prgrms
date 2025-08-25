cd7f082c007f391fa5f6858b7b5a198cc025fe53
katarianirvaans <katarianirvaans@gmail.com>
2025-08-25 15:31:11 +0530

    Create prgrm26.c
    26/∞

---
+++ prgrm26.c
@@ -0,0 +1,13 @@
+//Repeat 8 using while loop.
+
+#include<stdio.h>
+
+int main(){
+    int i=0;
+    while (1<12)
+    {
+        printf("Repeating 8 infinite times. \n");
+    }
+    
+    return 0;
+}
